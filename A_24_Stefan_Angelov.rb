2e4ee9dfa8dfa397d57d6dee9f0ed3dd689d99f1
Stefan Angelov <ste.angelov.tues19@gmail.com>
2017-10-16 13:25:20 +0300

    Create A_24_Stefan_Angelov.rb

---
+++ A_24_Stefan_Angelov.rb
@@ -0,0 +1,12 @@
+require 'csv'
+file = ARGV[0]
+print file.superclass
+
+#file_path = file.path
+=begin
+CSV.foreach(file_path) do |row|
+	heroku_path = row[4]
+	r1 = `curl -F "file=@/home/elsyser/11a/tp/hw1_tp.csv "` + heroku_path
+	puts r1
+end
+=end
