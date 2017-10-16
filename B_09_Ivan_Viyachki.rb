2485221b7c675f9995036c48fe8914eb4805a9bb
Ivan Viyachki <ivan_viyachki@abv.bg>
2017-10-16 12:37:25 +0300

    Update B_09_Ivan_Viyachki.rb

--- B_09_Ivan_Viyachki.rb
+++ B_09_Ivan_Viyachki.rb
@@ -3,12 +3,14 @@ require "CSV"
 student = CSV.read(ARGV[0])
 
 student.each do |row|
-
-	sum = `curl –form „file=@ /home/Desktop/Test.csv“ #{row[5]}/sums`  
-	filter = `curl –form „file=@/home/Desktop/Test.csv“ #{row[5]}/filter` 
-	interval = `curl –form „file=@/home/Desktop/Test.csv“ #{row[5]}/interval`
-	regresion = `curl –form „file=@/home/Desktop/Test.csv“ #{row[5]}/lin_regresion`  	
-
-
+t
+	sum = `curl –form "file=@ /home/Desktop/Test.csv" #{row[5]}/sums`.to_s 
+	filter = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/filter`.to_s 
+	interval = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/interval`.to_s
+	regresion = `curl –form "file=@/home/Desktop/Test.csv" #{row[5]}/lin_regresion` .to_s 	
+if(sum == "528.00" && filter == "272.00" && interval == "525.00" && regresion == "1.000000,0.000000")
+	p #{row[3]} #{row[4]} 1;
+end
+	
 end 	
 
